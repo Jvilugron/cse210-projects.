6eb5b063e041d8018c9240cd30e42427abdfe9e5
Joaquin Vilugron <joaquinvilugron@Joaquins-MacBook-Pro.local>
2023-09-16 19:03:33 -0600

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,70 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        // Determine the letter grade
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine if the user passed the course
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Your letter grade is {letter}. Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade is {letter}. Keep working for next time!");
+        }
+
+        // Stretch Challenge: Determine the grade sign
+        string sign = "";
+        int lastDigit = gradePercentage % 10;
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Handle exceptional cases for A, F grades
+        if (letter == "A" && sign == "+")
+        {
+            letter = "A+";
+            sign = "";
+        }
+        else if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        // Display both the grade letter and sign
+        Console.WriteLine($"Your final grade is {letter}{sign}");
     }
 }
