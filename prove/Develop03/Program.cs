547126f30a974a2e3385a79a7d869e9714c03699
Joaquin Vilugron <joaquinvilugron@Joaquins-MacBook-Pro.local>
2023-10-21 15:28:37 -0600

    develop03 done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to Scripture Memorization Program!");
+        Console.Write("Enter the scripture reference (e.g., 'John 3:16'): ");
+        string reference = Console.ReadLine();
+
+        Console.Write("Enter the scripture text: ");
+        string text = Console.ReadLine();
+
+        Scripture scripture = new Scripture(reference, text);
+
+        Console.Clear();
+        scripture.DisplayScripture();
+
+        while (true)
+        {
+            Console.WriteLine("Press Enter to hide a random word or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+            else
+                scripture.HideRandomWord();
+        }
     }
 }
